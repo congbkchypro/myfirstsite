ab4e865fb86c7c5476022cb8c432fbd9838436ed
congnm <cong12vn@gmail.com>
2018-10-25 19:32:29 +0700

    database and miigrations

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function index() {
+    	$project = Project::all();
+    	return $project;
+    	return view('projects.index');
+    }
+}
