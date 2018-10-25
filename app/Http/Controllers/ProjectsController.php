90f37c37fc75abb1fbafb665fc06c189bad0e013
congnm <cong12vn@gmail.com>
2018-10-25 20:02:19 +0700

    form handing, patch and post method

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -9,8 +9,46 @@ use Illuminate\Http\Request;
 class ProjectsController extends Controller
 {
     public function index() {
-    	$project = Project::all();
-    	return $project;
-    	return view('projects.index');
+    	$projects = Project::all();
+    	// return $project;
+    	return view('projects.index', compact('projects'));
+    }
+
+    public function create() {
+    	return view('projects.create');
+    }
+
+    public function show() {
+    	
+    }
+
+    public function store() {
+    	$project = new Project();
+    	$project->title = request()->title;
+    	$project->description = request()->description;
+    	$project->save();
+    	
+    	return redirect('/projects');
+    }
+
+    public function edit($id) {
+    	// return $id;
+    	$project = Project::find($id);
+    	// return $project;
+    	return view('projects.edit', compact('project'));
+    }
+
+    public function update($id, Request $req) {
+    	$project = Project::find($id);
+    	// return $req;
+		$project->title = request()->title;
+		$project->description = request()->description;
+		$project->save();
+
+		return redirect('/projects/' . $id . '/edit');
+    }
+
+    public function destroy() {
+    	
     }
 }
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -6,5 +6,9 @@
 </head>
 <body>
 	<h1>Project</h1>
+
+	@foreach($projects as $project)
+		<li>{{ $project->title }}</li>
+	@endforeach
 </body>
 </html>
